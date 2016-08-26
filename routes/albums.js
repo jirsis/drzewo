790e9ff8e942c2727d7d40d56b3c7ad3e3d15ee9
Iñaki Reta Sabarrós <jirsis@gmail.com>
2016-08-26 08:23:26 +0200

    Subida versión con promesa

--- routes/albums.js
+++ routes/albums.js
@@ -2,9 +2,10 @@ var debug = require('debug')('drzewo:router/albums');
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var exif = require('exif').ExifImage;
-var gm = require('gm');
+var Promise = require('bluebird');
+var gm = Promise.promisifyAll(require('gm'));
 var path = require('path');
+var async = require('async');
 
 router.get('/:name', function(req, res) {
 	var name = req.params.name;
@@ -12,11 +13,6 @@ router.get('/:name', function(req, res) {
 		if(err) res.status(500).send({"error": err});	
 		var data =[];
 		files.forEach(function(e){
-			// var imagen = {};
-			// imagen.href = "http://localhost:3000/img/"+name+"/"+e;
-			// imagen.type = 'image/jpeg';
-			// imagen.thumbnail = "http://localhost:3000/thumbnail/"+name+"/"+e;
-			// data.push(imagen);
 			data.push("http://jirsis.local:3000/img/"+name+"/"+e);
 		});
 		res.send(data);
@@ -43,25 +39,58 @@ router.post('/:name', function(req, res){
 			if(!path.basename(file).startsWith(".")) return file;
 		})
 		.map(function(file){
-			new exif({ image : file }, function (error, exifData) {
-				if (error) debug('Error: '+error.message);
-				else {
-					debug("+ "+file+": "+exifData.image.Orientation); 
-					gm(file)
-					.resize(75, 75)
-					.autoOrient()
-					.write(thumbnailDir+path.basename(file), function (err) {
-						if (err) thumbnailsCreated.push({failed: thumbnailDir+path.basename(file)});
-						else thumbnailsCreated.push({created: thumbnailDir+path.basename(file)});
-						//res.write({created: thumbnailDir+path.basename(file)});
-						debug("writer:" + thumbnailDir+path.basename(file));
-					});
-				}
-			});				
-		})
-		.reduce(function(){
-			res.send(thumbnailsCreated);	
+			gm(file)
+			.resize(75, 75)
+			.autoOrient()
+			.write(thumbnailDir+path.basename(file), function (err) {
+				if (err) thumbnailsCreated.push({failed: thumbnailDir+path.basename(file)});
+				else thumbnailsCreated.push({created: thumbnailDir+path.basename(file)});
+				debug("writer:" + thumbnailDir+path.basename(file));
+			});
 		});
+		res.send(thumbnailsCreated);
+	});
+})
+
+
+
+router.post('/jirsis/:name', function(req, res){
+	var name = req.params.name;
+	var dir = __dirname+'/../gallery/img/'+name;
+	var thumbnailDir= dir + "/thumbnail/";
+	fs.readdir(dir, function(err, files){
+		if(!fs.existsSync(thumbnailDir)){
+			fs.mkdirSync(thumbnailDir,0744);	
+		}
+		var thumbnailsCreated = [];
+
+		files = files.map(function(file){
+			return dir+'/'+file;
+		})
+		.filter(function(file){
+			if(!fs.statSync(file).isDirectory()) return file;
+		})
+		.filter(function(file){
+			if(!path.basename(file).startsWith(".")) return file;
+		});
+
+		Promise.map(files, function(file){
+			var gm = Promise.promisifyAll(require('gm')(file));
+			gm.writeAsync(thumbnailDir+path.basename(file))
+			.then(
+				function(){
+					debug("processed: "+thumbnailDir+path.basename(file));
+					thumbnailsCreated.push({created: thumbnailDir+path.basename(file)});
+				},
+				function(){
+					debug("error");
+					thumbnailsCreated.push({failed: thumbnailDir+path.basename(file)});
+				}
+			);
+		}).then(function(){
+			res.send(thumbnailsCreated);
+		});
+		
 	});
 })
 
