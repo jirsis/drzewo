c633638eb30a8b4ddbebc89d2b6a8ccdeec0533e
Raúl Gundín <raul@dmdes.com>
2016-08-26 11:07:45 +0200

    Versión de gm con promesas

--- routes/albums.js
+++ routes/albums.js
@@ -1,22 +1,24 @@
+var Promise = require('bluebird');
 var debug = require('debug')('drzewo:router/albums');
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var Promise = require('bluebird');
-var gm = Promise.promisifyAll(require('gm'));
+var fs = Promise.promisifyAll(require('fs'));
+var gm = require('gm');
+Promise.promisifyAll(gm.prototype);
 var path = require('path');
 var async = require('async');
+var sanitizeFilename = require('sanitize-filename');
 
 router.get('/:name', function(req, res) {
 	var name = req.params.name;
 	fs.readdir(__dirname+'/../gallery/img/'+name, function(err, files){
-		if(err) res.status(500).send({"error": err});	
+		if(err) res.status(500).send({"error": err});
 		var data =[];
 		files.forEach(function(e){
 			data.push("http://jirsis.local:3000/img/"+name+"/"+e);
 		});
 		res.send(data);
-	});	
+	});
 });
 
 
@@ -26,7 +28,7 @@ router.post('/:name', function(req, res){
 	var thumbnailDir= dir + "/thumbnail/";
 	fs.readdir(dir, function(err, files){
 		if(!fs.existsSync(thumbnailDir)){
-			fs.mkdirSync(thumbnailDir,0744);	
+			fs.mkdirSync(thumbnailDir,0744);
 		}
 		var thumbnailsCreated = [];
 		files.map(function(file){
@@ -60,7 +62,7 @@ router.post('/jirsis/:name', function(req, res){
 	var thumbnailDir= dir + "/thumbnail/";
 	fs.readdir(dir, function(err, files){
 		if(!fs.existsSync(thumbnailDir)){
-			fs.mkdirSync(thumbnailDir,0744);	
+			fs.mkdirSync(thumbnailDir,0744);
 		}
 		var thumbnailsCreated = [];
 
@@ -90,8 +92,57 @@ router.post('/jirsis/:name', function(req, res){
 		}).then(function(){
 			res.send(thumbnailsCreated);
 		});
-		
+
 	});
 })
+
+router.post('/promise/:name', function(req, res){
+	var name = sanitizeFilename(req.params.name);
+	var dir = path.normalize(
+		path.join(__dirname, '..', 'gallery', 'img', name)
+	);
+	var thumbnailDir = path.join(dir, 'thumbnail');
+
+	getFiles(dir)
+		.bind({ thumbnailDir: thumbnailDir, dir: dir })
+		.map(createThumbnail)
+		.bind(res)
+		.then(returnThumbnails);
+});
+
+function getFiles(dir) {
+	return fs.readdirAsync(dir).bind({ dir: dir }).then(filterFiles);
+}
+
+function filterFiles(files) {
+	var dir = this.dir;
+	return files.filter(function (file) {
+		return fs.statSync(path.join(dir, file)).isFile()
+			&& !file.startsWith('.')
+	});
+}
+
+function createThumbnail(file) {
+	var filePath = path.join(this.dir, file);
+	return gm(filePath)
+		.writeAsync(path.join(this.thumbnailDir, file))
+		.then(thumbOK.bind(this, filePath))
+		.catch(thumbKO);
+}
+
+function thumbOK(file) {
+	debug('processed: ', file);
+	return file;
+}
+
+function thumbKO(error) {
+	debug('error: ', error);
+	return;
+}
+
+function returnThumbnails(files) {
+	this.json({ thumbs: files });
+}
+
 
 module.exports = router;
