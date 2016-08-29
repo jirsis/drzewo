06d30290f7cdf06e40cc5a2cd043b60980c2346d
Iñaki Reta Sabarrós <jirsis@gmail.com>
2016-08-29 18:30:35 +0200

    Añadido el servicio de generación de thumbnails

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var debug = require('debug')('drzewo:app');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,6 +6,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
+
+var config = require('./config');
+
+var mongoose = require('mongoose');
+mongoose.connect(config.mongoHost);
+var db = mongoose.connection;
+db.on('error', function(){debug('conected to database FAILED: '+ config.mongoHost)});
+db.once('open', function(){debug('conected to database: '+ config.mongoHost)});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
