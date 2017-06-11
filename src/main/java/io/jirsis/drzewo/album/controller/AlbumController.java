c007064aac3008fc33482b417ab3df94cddff899
Iñaki Reta Sabarrós <jirsis@gmail.com>
2017-06-11 10:20:13 +0200

    Controller para crear nuevos álbumes

--- src/main/java/io/jirsis/drzewo/album/controller/AlbumController.java
+++ src/main/java/io/jirsis/drzewo/album/controller/AlbumController.java
@@ -1,6 +1,5 @@
 package io.jirsis.drzewo.album.controller;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.net.URI;
@@ -8,34 +7,40 @@ import java.net.URISyntaxException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jirsis.drzewo.album.service.AlbumService;
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class AlbumController {
+	
+	private AlbumService albumService;
 
-	@RequestMapping(path = AlbumLinks.ALBUM_NAME, method = GET)
-	public ResponseEntity<AlbumsResponse> getAllPhotosInAlbum(@PathVariable String name) {
-		AlbumsResponse response = new AlbumsResponse();
-		response.setAlbum(name);
-		response.setThumbnail(Boolean.FALSE);
-		return ResponseEntity.ok(response);
-	}
+//	@RequestMapping(path = AlbumLinks.ALBUM_NAME, method = GET)
+//	public ResponseEntity<AlbumsResponse> getAllPhotosInAlbum(@PathVariable String name) {
+//		AlbumsResponse response = new AlbumsResponse();
+//		response.setAlbum(name);
+//		response.setThumbnail(Boolean.FALSE);
+//		return ResponseEntity.ok(response);
+//	}
 
-	@RequestMapping(path = AlbumLinks.ALBUM_NAME_IMAGE, method = GET)
-	public ResponseEntity<AlbumsResponse> getSingleImage(@PathVariable("name") String album, @PathVariable String image) {
-		AlbumsResponse response = new AlbumsResponse();
-		response.setAlbum(album);
-		response.setImage(image);
-		response.setThumbnail(Boolean.FALSE);
-		return ResponseEntity.ok(response);
-	}
+//	@RequestMapping(path = AlbumLinks.ALBUM_NAME_IMAGE, method = GET)
+//	public ResponseEntity<AlbumsResponse> getSingleImage(@PathVariable("name") String album, @PathVariable String image) {
+//		AlbumsResponse response = new AlbumsResponse();
+//		response.setAlbum(album);
+//		response.setImage(image);
+//		response.setThumbnail(Boolean.FALSE);
+//		return ResponseEntity.ok(response);
+//	}
 
 	@RequestMapping(path = AlbumLinks.ALBUM_NAME, method = POST)
-	public ResponseEntity<AlbumsResponse> generateAlbum(@PathVariable("name") String album) throws URISyntaxException {
-		AlbumsResponse response = new AlbumsResponse();
-		response.setAlbum(album);
-		response.setThumbnail(Boolean.FALSE);
+	public ResponseEntity<NewAlbumResponse> generateAlbum(@PathVariable("name") String album, @RequestBody NewAlbumRequest request) throws URISyntaxException {
+		NewAlbumResponse response = albumService.createNewAlbum(album, request.getPath());
+		
 		return ResponseEntity.created(new URI(String.format("%s/%s", AlbumLinks.DEFAULT, album))).body(response);
 	}
 
---
+++ src/main/java/io/jirsis/drzewo/album/controller/NewAlbumRequest.java
@@ -0,0 +1,10 @@
+package io.jirsis.drzewo.album.controller;
+
+import lombok.Data;
+
+@Data
+public class NewAlbumRequest {
+	
+	private String path;
+
+}
